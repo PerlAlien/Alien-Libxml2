70a00d3adbaf2aa97465fa5ca32f19c4906a0233
Graham Ollis <perl@wdlabs.com>
2013-12-12 19:49:46 -0500

    doco

--- README.md
+++ README.md
@@ -2,6 +2,46 @@
 
 Install the C libxml2 library on your system
 
+# SYNOPSIS
+
+Build.PL
+
+    use Alien::Libxml2;
+    use Module::Build;
+    
+    my $alien = Alien::Libxml2;
+    my $build = Module::Build->new(
+      ...
+      extra_compiler_flags => $alien->cflags,
+      extra_linker_flags   => $alien->libs,
+      ...
+    );
+    
+    $build->create_build_script
+
+Makefile.PL
+
+    use Alien::Libxml2;
+    use ExtUtils::MakeMaker;
+    
+    my $alien = Alien::Libxml2->new;
+    WriteMakefile(
+      ...
+      CFLAGS => Alien::Libxml2->cflags,
+      LIBS   => Alien::Libxml2->libs,
+    );
+
+FFI
+
+    use Alien::Libxml2;
+    use FFI::Sweet qw( ffi_lib );
+    
+    ffi_lib(Alien::Libml2->new->libs);
+
+# DESCRIPTION
+
+This module provides libxml2 for other modules to use.
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Alien/Libxml2.pm
+++ lib/Alien/Libxml2.pm
@@ -8,6 +8,48 @@ use Text::ParseWords qw( shellwords );
 # ABSTRACT: Install the C libxml2 library on your system
 # VERSION
 
+=head1 SYNOPSIS
+
+Build.PL
+
+ use Alien::Libxml2;
+ use Module::Build;
+ 
+ my $alien = Alien::Libxml2;
+ my $build = Module::Build->new(
+   ...
+   extra_compiler_flags => $alien->cflags,
+   extra_linker_flags   => $alien->libs,
+   ...
+ );
+ 
+ $build->create_build_script
+
+Makefile.PL
+
+ use Alien::Libxml2;
+ use ExtUtils::MakeMaker;
+ 
+ my $alien = Alien::Libxml2->new;
+ WriteMakefile(
+   ...
+   CFLAGS => Alien::Libxml2->cflags,
+   LIBS   => Alien::Libxml2->libs,
+ );
+
+FFI
+
+ use Alien::Libxml2;
+ use FFI::Sweet qw( ffi_lib );
+ 
+ ffi_lib(Alien::Libml2->new->libs);
+
+=head1 DESCRIPTION
+
+This module provides libxml2 for other modules to use.
+
+=cut
+
 # workaround for Alien::Base gh#30
 sub import
 {
